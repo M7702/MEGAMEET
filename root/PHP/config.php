ff83cbb025ae2a6019968a102f15522206b8019b
MIT <MP7702524@GMAIL.COM>
2024-11-06 10:30:40 +0530

    adding env variable

--- root/PHP/config.php
+++ root/PHP/config.php
@@ -1,11 +1,18 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "megameet";
+// $host = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "megameet";
 // $port = "3307";
 
-$conn = new mysqli($host, $user, $password, $database);
+// $conn = new mysqli($host, $user, $password, $database);
 // $conn = mysqli_connect('localhost','root','','megameet');
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$database = getenv('DB_NAME');
+
+$conn = new mysqli($host, $username, $password, $database);
+
 
 ?>
